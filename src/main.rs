4be6091f5da7b51183ba5b723cf36d001ee84f9a
net3i <web3ten0@gmail.com>
2023-11-08 19:22:06 +0900

    added clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+/// Repo Condenser - A CLI tool to efficiently condense repository files
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the repository's root directory
+    #[clap(value_parser)]
+    path_to_repo: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    // Display the input path provided by the user
+    println!("Repository Path: {}", args.path_to_repo);
+
+    // TODO: Implement the file size calculation logic here
 }
