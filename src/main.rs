ea7d9fe6e321ceeb80b246c076f2cafcd39a5dda
net3i <web3ten0@gmail.com>
2023-11-08 21:42:20 +0900

    calc total files size

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
 use clap::Parser;
+use ignore::WalkBuilder;
+use std::io;
+
+/// Recursively calculates the total size of all files in a directory.
+fn get_dir_size(dir: &str) -> io::Result<u64> {
+    let mut total_size: u64 = 0;
+    let walker = WalkBuilder::new(dir).build(); // `.gitignore` is automatically respected
+
+    for result in walker {
+        if let Ok(entry) = result {
+            if entry.file_type().map_or(false, |ft| ft.is_file()) {
+                if let Ok(metadata) = entry.metadata() {
+                    total_size += metadata.len();
+                }
+            }
+        }
+    }
+
+    Ok(total_size)
+}
 
 /// Repo Condenser - A CLI tool to efficiently condense repository files
 #[derive(Parser, Debug)]
@@ -9,11 +29,11 @@ struct Args {
     path_to_repo: String,
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    // Display the input path provided by the user
-    println!("Repository Path: {}", args.path_to_repo);
+    let total_size = get_dir_size(&args.path_to_repo)?;
+    println!("Total size of non-ignored files: {} bytes", total_size);
 
-    // TODO: Implement the file size calculation logic here
+    Ok(())
 }
