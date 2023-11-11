9d3238d5b6efc5434d7862c0aa02338081a952b5
edo1z <endo.yuta@logicky.com>
2023-11-11 16:04:50 +0900

    deleted repository name

--- src/output_formatting.rs
+++ src/output_formatting.rs
@@ -1,10 +1,7 @@
-pub fn create_page_header(repository_name: &str, file_path: &str, page_number: u64) -> String {
+pub fn create_page_header(file_path: &str, page_number: u64) -> String {
     format!(
-        "# repcon_repository_name: {}\n# repcon_file_name: {}\n# repcon_page_number: {}\n// START OF CODE BLOCK: {}\n",
-        repository_name,
-        file_path,
-        page_number,
-        file_path
+        "# repcon_file_name: {}\n# repcon_page_number: {}\n// START OF CODE BLOCK: {}\n",
+        file_path, page_number, file_path
     )
 }
 
