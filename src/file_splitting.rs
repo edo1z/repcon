1e52d1ce7e6c396f58151d9111a4d416dc1e508e
edo1z <endo.yuta@logicky.com>
2023-11-11 22:18:06 +0900

    Refactoring of file_splitting.rs and output_formatting.rs

--- src/file_splitting.rs
+++ src/file_splitting.rs
@@ -1,4 +1,4 @@
-use crate::output_formatting::{create_page_footer, create_page_header};
+use crate::PageFormat;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Write};
 use std::path::{Path, PathBuf};
@@ -14,6 +14,18 @@ fn create_new_file(
     Ok((file, file_path))
 }
 
+fn check_max_file_size(page_format: &PageFormat, max_file_size: u64) -> io::Result<()> {
+    if page_format.get_page_header_size() + page_format.get_page_footer_size() > max_file_size {
+        let error_message = format!(
+            "Error: The maximum file size ({}) is too small to contain the page header and footer.",
+            max_file_size
+        );
+        Err(io::Error::new(io::ErrorKind::InvalidData, error_message))
+    } else {
+        Ok(())
+    }
+}
+
 pub fn split_files_into_chunks(
     files: &[PathBuf],
     output_directory: &Path,
@@ -23,32 +35,28 @@ pub fn split_files_into_chunks(
     let mut generated_files = Vec::new();
     let mut file_counter: u64 = 1;
     let mut current_file_size: u64 = 0;
-    let mut current_file_name = "";
-    let mut page_number: u64;
+    let mut current_file_name: &str;
+    let mut page_format: PageFormat;
+
+    // Create the first file
     let (mut output_file, mut output_file_path) =
         create_new_file(output_directory, file_counter, output_name)?;
     generated_files.push(output_file_path);
-    let mut page_header: String;
-    let mut page_header_size: u64;
-    let page_footer = create_page_footer(current_file_name);
-    let page_footer_size = page_footer.as_bytes().len() as u64;
 
     for file_path in files {
-        current_file_name = file_path.to_str().unwrap(); // Safely convert PathBuf to &str
-        page_number = 1;
-        page_header = create_page_header(current_file_name, page_number);
-        page_header_size = page_header.as_bytes().len() as u64 + 1; // +1 for the newline character
+        current_file_name = match file_path.to_str() {
+            Some(name) => name,
+            None => {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "File path contains invalid UTF-8 characters",
+                ));
+            }
+        };
+        page_format = PageFormat::new(current_file_name);
+        check_max_file_size(&page_format, max_file_size)?;
 
-        if page_header_size + page_footer_size > max_file_size {
-            eprintln!(
-                "Error: The maximum file size ({}) is too small to contain the page header and footer.",
-                max_file_size
-            );
-            std::process::exit(1);
-        }
-
-        if current_file_size + page_header_size > max_file_size {
-            // Finish the current file and create a new one
+        if current_file_size + page_format.header_size > max_file_size {
             file_counter += 1;
             current_file_size = 0;
             (output_file, output_file_path) =
@@ -56,8 +64,8 @@ pub fn split_files_into_chunks(
             generated_files.push(output_file_path);
             continue;
         } else {
-            writeln!(output_file, "{}", page_header)?;
-            current_file_size += page_header_size;
+            writeln!(output_file, "{}", page_format.header)?;
+            current_file_size += page_format.header_size;
         }
 
         let file = File::open(file_path)?;
@@ -66,38 +74,25 @@ pub fn split_files_into_chunks(
             let line = line?;
             let line_size = line.as_bytes().len() as u64 + 1; // +1 for the newline character
 
-            // Check if the current line can be added to the current file
-            if current_file_size + line_size + page_footer_size > max_file_size {
-                // Write the footer before closing the current file
-                write!(output_file, "{}", create_page_footer(current_file_name))?;
+            if current_file_size + line_size + page_format.footer_size > max_file_size {
+                write!(output_file, "{}", page_format.footer)?;
 
-                // Finish the current file and create a new one
                 file_counter += 1;
                 current_file_size = 0;
                 (output_file, output_file_path) =
                     create_new_file(output_directory, file_counter, output_name)?;
                 generated_files.push(output_file_path);
 
-                // Write the header to the new file
-                page_number += 1;
-                page_header = create_page_header(current_file_name, page_number);
-                page_header_size = page_header.as_bytes().len() as u64 + 1; // +1 for the newline character
-                if page_header_size + page_footer_size > max_file_size {
-                    eprintln!(
-                        "Error: The maximum file size ({}) is too small to contain the page header and footer.",
-                        max_file_size
-                    );
-                    std::process::exit(1);
-                }
-                writeln!(output_file, "{}", page_header)?;
-                current_file_size += page_header_size;
+                page_format.increment_page_number();
+                check_max_file_size(&page_format, max_file_size)?;
+                writeln!(output_file, "{}", page_format.header)?;
+                current_file_size += page_format.header_size;
             }
 
             writeln!(output_file, "{}", line)?;
             current_file_size += line_size;
         }
-        // Don't forget to add a footer when you finish writing a file
-        write!(output_file, "{}", create_page_footer(current_file_name))?;
+        write!(output_file, "{}", page_format.footer)?;
     }
     Ok(generated_files)
 }
